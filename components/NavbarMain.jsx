019ee3dfe9483ca67e7ab190c6cec81c93e2637d
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-26 22:14:41 -0500

    Update

--- components/NavbarMain.jsx
+++ components/NavbarMain.jsx
@@ -13,6 +13,22 @@ const NavbarMain = () => {
 
   return (
     <div className={styles.header_content}>
+      <div className={styles.div_promo}>
+        HOT SALE -30% EN SANDALIAS
+      </div>
+      <div className={styles.content_micuenta}>
+        <div className={styles.div_menu_micuenta}>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+              
+            </div>
+        </div>
+      </div>
       
     </div>
   );
