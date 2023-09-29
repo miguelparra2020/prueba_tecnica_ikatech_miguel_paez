4dfaf420633fe59974b4aa9ff89c4443c66ed2ee
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-28 22:17:14 -0500

    Update

--- components/NavbarMain.jsx
+++ components/NavbarMain.jsx
@@ -48,7 +48,7 @@ const NavbarMain = () => {
                       </svg>
                     </div>
                   </div>
-                  <div className={styles.div_card_movil}>
+                  <div className={styles.div_car_movil}>
                       <div>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
   <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
@@ -70,7 +70,39 @@ const NavbarMain = () => {
           </div>
       </div>
       <div className={styles.content_menu}>
+        <div className={styles.div_menu_carrito}>
+          <div className={styles.div_menu}>
+              <div>
+                HOMBRE
+              </div>
+              <div>
+                MUJER
+              </div>
+              <div>
+                BLOG
+              </div>
+              <div> 
+                HISTORIA
+              </div>
+              <div>
+                TIENDAS
+              </div>
 
+          </div>
+          <div  className={styles.div_carrito}>
+            <div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+              </svg>
+            </div>&nbsp;&nbsp;
+            <div>
+              CARRITO
+            </div>&nbsp;&nbsp;
+            <div>
+              {cantidadCarrito}
+            </div>
+          </div>
+        </div>
       </div>
       
     </div>
