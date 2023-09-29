e8b5d2361a300e28bd6739e0f4ef5049e8db6b02
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-09-28 20:06:35 -0500

    Update

--- components/NavbarMain.jsx
+++ components/NavbarMain.jsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import styles from '../styles/NavbarMain.module.scss';
 
 const NavbarMain = () => {
+  const [cantidadCarrito, setCantidadCarrito] = useState(0);
+  
   useEffect(() => {
   
   }, []);
@@ -34,7 +36,7 @@ const NavbarMain = () => {
       <div className={styles.content_logo_buscador_envio}>
           <div className={styles.div_logo_buscador_envio}>
             <div className={styles.div_logo_tipo}>
-                  <Image src="/img/21.jpg" width={300} height={80} alt='Imagen Logo Tipo'/>
+                  <Image src="/img/21.jpg" width={300} height={80} alt='Imagen Logo Tipo' className={styles.img_logotipo}/>
             </div>
             <div className={styles.div_buscador_envio}>
                 <div className={styles.div_buscador}>
@@ -45,6 +47,20 @@ const NavbarMain = () => {
     <path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
                       </svg>
                     </div>
+                  </div>
+                  <div className={styles.div_card_movil}>
+                      <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+</svg>
+                      </div>&nbsp;&nbsp;
+                      <div>
+                        CARRITO
+                      </div>&nbsp;&nbsp;
+                      <div>
+                        {cantidadCarrito}
+                      </div>
+                      
                   </div>
                 </div>
                 <div className={styles.div_envio}>
