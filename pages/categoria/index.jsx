75110bcbca1f9e27dae3be71711a3410647be768
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-10-01 10:14:28 -0500

    Update

--- pages/categoria/index.jsx
+++ pages/categoria/index.jsx
@@ -1,6 +1,7 @@
 // pages/index.js
 import React, { useEffect } from 'react';
 import  MainLayout from '../../components/layouts/MainLayout';
+import styles from '../../styles/'
 import Image from 'next/image';
 
 import { Toaster, toast } from 'sonner';
@@ -11,7 +12,24 @@ const CategoriaPage = () => {
   }, []);
   return (
     <MainLayout>
-     PÁGINA CATEGORIA
+     <div className={styles.content_indicador_ruta}>
+
+     </div>
+     <div className={styles.content_fotos_talla}>
+
+     </div>
+     <div className={styles.content_detalles_producto}>
+
+     </div>
+     <div className={styles.content_tecnologias}>
+
+     </div>
+     <div className={styles.content_completa_look}>
+
+     </div>
+     <div className={styles.content_productos_recomendador}>
+
+     </div>
     </MainLayout>
   );
 };
