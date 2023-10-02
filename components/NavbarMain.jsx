9d45534907993fba827586513e2a1fdcfa7890ae
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2023-10-02 12:14:17 -0500

    Update

--- components/NavbarMain.jsx
+++ components/NavbarMain.jsx
@@ -4,8 +4,17 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/NavbarMain.module.scss';
 import CounterValue from './CounterValue';
+import { Toaster, toast } from 'sonner';
 
 const NavbarMain = () => {
+  const [valorBusqueda, setBuscar] = useState('');
+  const handlBuscar = (e) => {
+    e.preventDefault();
+    toast.success('Producto buscado:', {
+      description: valorBusqueda
+    });
+    setBuscar('');    
+  }
   
   useEffect(() => {
   
@@ -51,8 +60,8 @@ const NavbarMain = () => {
             <div className={styles.div_buscador_envio}>
                 <div className={styles.div_buscador}>
                   <div className={styles.div_input_icon_buscar}>
-                    <input type="text" placeholder='  BUSCAR' className={styles.input_buscar}/>
-                    <div className={styles.div_buscador_icon}>
+                    <input type="text" placeholder='  BUSCAR' className={styles.input_buscar} value={valorBusqueda}  onChange={(e) => setBuscar(e.target.value)} />
+                    <div className={styles.div_buscador_icon} onClick={handlBuscar}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-right" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"/>
                       </svg>
@@ -133,7 +142,7 @@ const NavbarMain = () => {
           </div>          
         </div>
       </div>
-      
+      <Toaster/>
     </div>
   );
 };
